e2224b38b60e39a3cec2900a409369b1d02177a3
Yasser hassan ikuzwe mugisha <1099664@pdsb.net>
2026-07-09 18:30:07 -0400

    Update Discord client configuration and token handling

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
 require("dotenv").config();
-const config = require("./config");
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions
     ],
-    partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildMember]
 });
 
 client.commands = new Collection();
 client.cooldowns = new Collection();
 
-// Load handlers
 ["commandHandler", "eventHandler", "interactionHandler"].forEach((handler) => {
-    require(`./src/handlers/${handler}`)(client);
+    require("./src/handlers/" + handler)(client);
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
